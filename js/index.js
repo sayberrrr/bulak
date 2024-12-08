6adc4164c56a777ae00cdcabddcee97b738aca39
sayberrrr <zyverfaelnar123@gmail.com>
2024-12-08 13:22:16 -0500

    hmm

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'nakoy iingon saimo'.split('')
+const text = 'Hi disney princess'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('tang ina mo hotnose').split('')
+    const titles = ('kitty putotflower').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
